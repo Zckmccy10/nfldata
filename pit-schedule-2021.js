9e1851caa714ec7cbe7caaf2ab36475e14d1690b
Zckmccy10 <82622251+Zckmccy10@users.noreply.github.com>
2021-05-30 17:02:45 -0500

    Add files via upload

---
+++ pit-schedule-2021.js
@@ -0,0 +1,228 @@
+export const schedule = {
+  preseason: [
+    {
+      id: p1,
+      week: 1,
+      opponent: "DAL",
+      home: false,
+      date: new Date("August 8, 2021 19:00"),
+      venue: "Tom Benson Stadium",
+      homeScore: "",
+      awayScore: ""
+    },
+    {
+      id: p2,
+      week: 2,
+      opponent: "PHI",
+      home: false,
+      date: new Date("August 12, 2021 18:30"),
+      venue: "Lincoln Financial Field",
+      homeScore: "",
+      awayScore: ""
+    },
+    {
+      id: p3,
+      week: 3,
+      opponent: "DET",
+      home: true,
+      date: new Date("August 21, 2021 18:30"),
+      venue: "Heinz Field",
+      homeScore: "",
+      awayScore: ""
+    },
+    {
+      id: p4,
+      week: 4,
+      opponent: "CAR",
+      home: false,
+      date: new Date("August 27, 2021 18:30"),
+      venue: "Bank of America Stadium",
+      homeScore: "",
+      awayScore: ""
+    }
+],
+season: [
+      {
+        id: w1,
+        week: 1,
+        opponent: "BUF",
+        home: false,
+        date: new Date("September 12, 2021 12:00"),
+        venue: "Highmark Stadium",
+        homeScore: "",
+        awayScore: ""
+      },
+      {
+        id: w2,
+        week: 2,
+        opponent: "LVR",
+        home: true,
+        date: new Date("September 19, 2021 12:00"),
+        venue: "Heinz Field",
+        homeScore: "",
+        awayScore: ""
+      },
+      {
+        id: w3,
+        week: 3,
+        opponent: "CIN",
+        home: true,
+        date: new Date("September 26, 2021 12:00"),
+        venue: "Heinz Field",
+        homeScore: "",
+        awayScore: ""
+      },
+      {
+        id: w4,
+        week: 4,
+        opponent: "GBP",
+        home: false,
+        date: new Date("October 3, 2021 15:25"),
+        venue: "Lambeau Field",
+        homeScore: "",
+        awayScore: ""
+      },
+      {
+        id: w5,
+        week: 5,
+        opponent: "DEN",
+        home: true,
+        date: new Date("October 10, 2021 12:00"),
+        venue: "Heinz Field",
+        homeScore: "",
+        awayScore: ""
+      },
+      {
+        id: w6,
+        week: 6,
+        opponent: "SEA",
+        home: true,
+        date: new Date("October 17, 2021 19:20"),
+        venue: "Heinz Field",
+        homeScore: "",
+        awayScore: ""
+      },
+      {
+          id: w7,
+          week: 7,
+          opponent: "BYE",
+          home: true,
+          date: new Date("October 24, 2021 12:00"),
+          venue: "Heinz Field",
+          homeScore: "",
+          awayScore: ""
+        },
+        {
+          id: w8,
+          week: 8,
+          opponent: "CLE",
+          home: false,
+          date: new Date("October 31, 2021 12:00"),
+          venue: "First Energy Stadium",
+          homeScore: "",
+          awayScore: ""
+        },
+        {
+          id: w9,
+          week: 9,
+          opponent: "CHI",
+          home: true,
+          date: new Date("November 08, 2021 19:15"),
+          venue: "Heinz Field",
+          homeScore: "",
+          awayScore: ""
+        },  
+        {
+          id: w10,
+          week: 10,
+          opponent: "DET",
+          home: true,
+          date: new Date("November 14, 2021 12:00"),
+          venue: "Heinz Field",
+          homeScore: "",
+          awayScore: ""
+        },  
+        {
+          id: w11,
+          week: 11,
+          opponent: "LAC",
+          home: false,
+          date: new Date("November 21, 2021 19:20"),
+          venue: "SoFi Stadium",
+          homeScore: "",
+          awayScore: ""
+        },  
+        {
+          id: w12,
+          week: 12,
+          opponent: "CIN",
+          home: false,
+          date: new Date("November 28, 2021 12:00"),
+          venue: "Paul Brown Stadium",
+          homeScore: "",
+          awayScore: ""
+        },  
+        {
+          id: w13,
+          week: 13,
+          opponent: "BAL",
+          home: true,
+          date: new Date("December 5, 2021 15:25"),
+          venue: "Heinz Field",
+          homeScore: "",
+          awayScore: ""
+        },
+        {
+          id: w14,
+          week: 14,
+          opponent: "MIN",
+          home: false,
+          date: new Date("December 9, 2021 19:20"),
+          venue: "U.S. Bank Stadium",
+          homeScore: "",
+          awayScore: ""
+        },
+        {
+          id: w15,
+          week: 15,
+          opponent: "TEN",
+          home: true,
+          date: new Date("December 19, 2021 12:00"),
+          venue: "Heinz Field",
+          homeScore: "",
+          awayScore: ""
+        },
+        {
+          id: w16,
+          week: 16,
+          opponent: "KCC",
+          home: false,
+          date: new Date("December 26, 2021 15:25"),
+          venue: "Arrowhead Stadium",
+          homeScore: "",
+          awayScore: ""
+        },
+        {
+          id: w17,
+          week: 17,
+          opponent: "CLE",
+          home: true,
+          date: new Date("January 3, 2022 19:15"),
+          venue: "Heinz Field",
+          homeScore: "",
+          awayScore: ""
+        },
+        {
+          id: w18,
+          week: 18,
+          opponent: "BAL",
+          home: false,
+          date: new Date("January 9, 2022 12:00"),
+          venue: "M&T Bank Stadium",
+          homeScore: "",
+          awayScore: ""
+        },
+  ],
+  postseason: [],
+};
+
